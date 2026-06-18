f5788037d7326d30b21dc52c081406dc4c4b627d
Shaik Sajida <anwarjhanshaik@gmail.com>
2026-06-18 11:40:28 +0530

    Add email masking function to first_repo.js

--- first_repo.js
+++ first_repo.js
@@ -1,1 +1,11 @@
 
+const email = "anwrjhanshaik@gmail.com";
+function emailMasker(email) {
+  const index = email.indexOf("@")-1;
+  const portion = email.slice(1, index).length;
+  const masked_email = email[0] + "*".repeat(portion) + email.slice(index);
+  return masked_email;
+}
+
+const func_call = emailMasker(email);
+console.log(func_call);
